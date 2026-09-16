ac55e91e4ccd8f1c8acfbcc3191a4102cabe7b11
tang1811 <eecprogrammer@gmail.com>
2026-09-16 09:01:19 +0700

    Style contact bubbles by brand and remove legacy Next.js app

--- web/app/css-vars.d.ts
+++
@@ -1,8 +0,0 @@
-import 'react';
-
-// Allow CSS custom properties (--foo) in inline style objects project-wide.
-declare module 'react' {
-  interface CSSProperties {
-    [key: `--${string}`]: string | number | undefined;
-  }
-}
